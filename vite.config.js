a7170d40706cc6b5d100c108d62130f32429dab5
abdennour <abdoufuntuber@gmail.com>
2025-08-05 12:17:24 +0200

    6th commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: ''
-})
+export default defineConfig(({ command }) => {
+  // Use '/' for development and the repository name for production
+  const base = command === 'build' ? '/aurora-lumen-lp/' : '/';
+
+  return {
+    plugins: [react()],
+    base: base,
+  };
+});
